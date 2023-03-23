09bba755ecd855bb53b02a7f7510bdfc00253fe6
pbpl1 <pbpl123456@gmail.com>
2023-03-23 22:59:33 +0530

    pivot elem

---
+++ src/main/java/Easy/Find_Pivot_Index.java
@@ -0,0 +1,23 @@
+package Easy;
+
+public class Find_Pivot_Index {
+
+    public int pivotIndex(int[] nums) {
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            rightSum += nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            rightSum -= nums[i];
+            if (rightSum - leftSum == 0) {
+                return i;
+            }
+            leftSum += nums[i];
+
+        }
+        return -1;
+
+    }
+}
+
